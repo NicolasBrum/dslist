9c6afec3c3b04fcf0192636c9453ef45249fcc69
Nicolas Brum <nickormy@gmail.com>
2024-11-15 19:49:46 -0300

    feat: domain model implemented

---
+++ src/main/java/com/projetoapi/dslist/entities/Belonging.java
@@ -0,0 +1,36 @@
+package com.projetoapi.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging() {}
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/projetoapi/dslist/entities/GameList.java
@@ -0,0 +1,40 @@
+package com.projetoapi.dslist.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+    @Id
+    private Long id;
+    private String nome;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
