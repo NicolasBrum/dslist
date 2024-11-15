875c30090a1f851adcba9babf2bd3d136185864f
Nicolas Brum <nickormy@gmail.com>
2024-11-15 16:40:25 -0300

    feat: properties, Game, ORM created

---
+++ src/main/java/com/projetoapi/dslist/entities/Game.java
@@ -0,0 +1,54 @@
+package com.projetoapi.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String plataforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {}
+
+    public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+                String longDescription, String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.plataforms = plataforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.longDescription = longDescription;
+        this.shortDescription = shortDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
